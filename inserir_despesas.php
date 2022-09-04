b51783b837793fcc681ead96ebbc1e7fd5404d8d
vitorfail <vitor_andrademanoel@hotmail.com>
2022-09-04 15:53:23 -0300

    weds

--- inserir_despesas.php
+++ inserir_despesas.php
@@ -13,15 +13,22 @@
                 try{
                     include_once('conexao.php');
                     $dados_de_usuario_sql = AuthController::dados_de_sql(); 
-                    
-                    $venda = "INSERT INTO `user_despesas` (user_id, descricao,valor_despesas, data_vencimento, data_pagamento) 
-                    VALUES (:user_id, :descricao ,:valor_despesas, :data_vencimento, :data_pagamento)";
+                    $situacao = '';
+                    if($_POST['situacao'] == false){
+                        $situacao = "Aberto";
+                    }
+                    else{
+                        $situacao = "Pago";
+                    }
+                    $venda = "INSERT INTO `user_despesas` (user_id, descricao,valor_despesas, data_vencimento, data_pagamento, situacao) 
+                    VALUES (:user_id, :descricao ,:valor_despesas, :data_vencimento, :data_pagamento, :situacao)";
                     $inserir_venda = $conexao->prepare($venda);
                     $inserir_venda->bindValue( ':user_id', $dados_de_usuario_sql->id);
                     $inserir_venda->bindValue(':descricao', $_POST['nome']);
                     $inserir_venda->bindValue(':valor_despesas', floatval($_POST['valor']));
                     $inserir_venda->bindValue(':data_vencimento', $_POST['data_vencimento']);
                     $inserir_venda->bindValue(':data_pagamento', $_POST['data']);
+                    $inserir_venda->bindValue(':situacao', $situacao);
                     $inserir_venda->execute();
                     
                     $add = "UPDATE `user_financeiro` SET caixa = caixa - :caixa WHERE id= :id";
